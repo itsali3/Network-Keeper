b39b3a18a322f84348cc2f595d70811ed44394fd
Ali Sammour <34039962+itsali3@users.noreply.github.com>
2018-01-25 17:30:53 -0500

    Update NetworkKeeper.sh

--- NetworkKeeper.sh
+++ NetworkKeeper.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
-INPUT_STRING=hello
-while [ "$INPUT_STRING" != "bye" ]
-do
-  ping -c 1 google.com
+INPUT_STRING=hello | \
+#Initiating a never ending loop to take ping
+while [ "$INPUT_STRING" != "bye" ] 
+do | \
+  #Using ping command to check if there is connection between 
+  ping -c 1 google.com | \
+  #If a connection is present, will display the ping stats, print the Network is up, and pause for 5 seconds before the next connection check
   if [ $? -eq 0 ]; 
   then 
-    sleep 5s
     echo
     echo "~~~~NETWORK IS ONLINE~~~~~";
     echo
+    sleep 5s | \
+    #If the connection is not established (network is down), will open firefox, run the macro to reestablish network, and pause for 5 seconds before the next connection check
   else
     export DISPLAY=:0
     xhost localhost
-    echo Need to boot up
-    xdg-open 'imacros://run/?m=%23Current.iim'
+    echo Need to reconnect | \
+    #Set this link to the bookmarked macro (Make sure to check out the README.md file)
+    xdg-open 'imacros://run/?m=YOURMACRO.iim'
     sleep 5s	
   fi
 done
